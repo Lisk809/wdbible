614fad2f5b125eab51906775d224e71a567be447
Lisk <liskqwq@gmail.com>
2023-08-13 15:08:07 +0800

    chore: prettier code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const axios=require("axios")
-const fs=require("fs")
-async function main(){
-  let r=await axios.get("https://wdbible.app/app")
-  let reg=/<a href="(.*?)">/igm
-  let url=r.data.match(reg)[15].replace(reg, "$1")
-  console.log(url)
+const axios = require("axios");
+const fs = require("fs");
+async function main() {
+  let r = await axios.get("https://wdbible.app/app");
+  let reg = /<a href="(.*?)">/gim;
+  let url = r.data.match(reg)[15].replace(reg, "$1");
+  console.log(url);
   // fs.writeFileSync("url.txt", url)
 }
-main()
+main();
